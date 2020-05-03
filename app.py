10c451bd1265304c04808c7652d51455f39b1198
cuoxin <1984332305@qq.com>
2020-05-03 20:14:42 +0800

    Add index

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html', name = name, movies = movies)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return "User : {}".format(name)
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', name='Adrian'))
-    print(url_for('test_url_for'))
-    print(url_for('test_url_for', num=2))
+## 虚拟数据
+name = 'Adrian'
+movies = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
+]
